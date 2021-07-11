687fee9b817b153cef6fb0effe86020eaea47602
satokouhei9649 <77945717+satokouhei9649@users.noreply.github.com>
2021-07-11 11:33:50 +0900

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -1,21 +1,21 @@
 'use strict'; 
 {
-const mysite = document.getElementById('mysite')
-const show = document.getElementById('show')
-const close = document.getElementById('btn')
-const mask = document.getElementById('mask')
+const mysite = document.getElementById('mysite');
+const show = document.getElementById('show');
+const close = document.getElementById('btn');
+const mask = document.getElementById('mask');
+const modal = document.getElementById('modal');
 
 mysite.addEventListener('click', () => {
-  show.classList.remove('hidden');
+  modal.classList.remove('hidden');
   mask.classList.remove('hidden');
 });
 close.addEventListener('click', () => {
-  show.classList.add('hidden');
+  modal.classList.add('hidden');
   mask.classList.add('hidden');
 });
 mask.addEventListener('click', () => {
-  show.classList.add('hidden');
-  mask.classList.add('hidden');
+  close.click();
 });
 
 
