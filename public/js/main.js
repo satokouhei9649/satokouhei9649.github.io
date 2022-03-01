1ff5c44ea5b93f28875d057b3892298d4e48aaff
佐藤　航平 <77945717+satokouhei9649@users.noreply.github.com>
2022-03-01 20:28:51 +0900

    修正

--- public/js/main.js
+++ public/js/main.js
@@ -5,9 +5,12 @@ const show = document.getElementById('show');
 const close = document.getElementById('btn');
 const mask = document.getElementById('mask');
 const modal = document.getElementById('modal');
-const myapp_btn = document.querySelector('.myapp_btn');
-const myapp_modal = document.getElementById('modal-work1');
-const myapp_back = document.querySelector('.myapp_back');
+const myapp1_btn = document.querySelector('.myapp1_btn');
+const myapp2_btn = document.querySelector('.myapp2_btn');
+const myapp_modal1 = document.getElementById('modal-work1');
+const myapp_modal2 = document.getElementById('modal-work2');
+const myapp1_back = document.querySelector('.myapp1_back');
+const myapp2_back = document.querySelector('.myapp2_back');
 
 mysite.addEventListener('click', () => {
   modal.classList.remove('hidden');
@@ -20,11 +23,20 @@ close.addEventListener('click', () => {
 mask.addEventListener('click', () => {
   close.click();
 });
-myapp_btn.addEventListener('click', ()=> {
-  myapp_modal.classList.remove('hidden');
+myapp1_btn.addEventListener('click', ()=> {
+  myapp_modal1.classList.remove('hidden');
+  mask.classList.remove('hidden');
 });
-myapp_back.addEventListener('click', ()=> {
-  myapp_modal.classList.add('hidden');
+myapp2_btn.addEventListener('click', ()=> {
+  myapp_modal2.classList.remove('hidden');
+  mask.classList.remove('hidden');
+});
+myapp1_back.addEventListener('click', ()=> {
+  myapp_modal1.classList.add('hidden');
+  mask.classList.add('hidden');
+})
+myapp2_back.addEventListener('click', ()=> {
+  myapp_modal2.classList.add('hidden');
   mask.classList.add('hidden');
 })
 
